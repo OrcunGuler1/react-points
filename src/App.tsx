c86f905ead71c170d021379b3b146e6ed7393da1
Orcun Guler <orcunguler381@gmail.com>
2022-11-14 23:19:56 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+type Point = {
+  x: number
+  y: number
+}
+
+function App() {
+  const [points, setPoints] = useState<Point[]>([])
+  const [removedPoints, setRemovedPoints] = useState<Point[]>([])
+  const handleMouseClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = e
+    setPoints(prev => [...prev, { x: clientX, y: clientY }])
+  }
+
+  const handleUndo = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    setPoints(prev => {
+      const lastPoint = prev[prev.length - 1]
+      setRemovedPoints(prev => [...new Set([...prev, lastPoint])])
+      return prev.slice(0, -1)
+    })
+  }
+  const handleRedo = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    setRemovedPoints(prev => {
+      const lastPoint = prev[prev.length - 1]
+      setPoints(prev => [...prev, lastPoint])
+      return prev.slice(0, -1)
+    })
+  }
+  return (
+    <div className="App" onClick={handleMouseClick}>
+      <button
+        onClick={handleUndo}
+        disabled={points.length === 0}
+        className="undo"
+      >
+        Undo
+      </button>
+      <button
+        onClick={handleRedo}
+        disabled={removedPoints.length === 0}
+        className="redo"
+      >
+        Redo
+      </button>
+      {points.map((point, i) => (
+        <div
+          key={i}
+          className="point"
+          style={{
+            left: point.x,
+            top: point.y,
+          }}
+        ></div>
+      ))}
+    </div>
+  )
+}
+
+export default App
